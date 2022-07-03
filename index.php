2f63e645b735386c973a628d91cec1ac006dcc5b
essigm01 <105976662+essigm01@users.noreply.github.com>
2022-07-02 21:11:13 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <title>Index-1a</title>
+      <style>
+        .container {
+          width: 1000px;
+          margin-left: auto;
+          margin-right: auto;
+        }
+        .content {
+          background-color: #66FF33;
+          width: auto;
+          text-align: center;
+        }
+        .contact {
+          background-color: inherit;
+          width: 100%;
+          text-align: center;
+        }
+        .container1 {
+          width: 2000px;
+          height: 1000px;
+          margin-left: auto;
+          margin-right: auto;
+        }
+        .container1 image {
+          width: auto;
+          height: auto;
+          display: block;
+        }
+      .content1 {
+        background-color: white;
+        shape-image-threshold:
+        width: auto;
+        text-align: center;
+      }
+    </style>
+    <meta charset="UTF-8" />
+<body> <!--This is where all our content will go-->
+  <div class="container">
+    <div class="content">
+      <h1 id="my-name">Rich Essigmann</h1>
+      <p class="text">I'm <b>Retired</b> Military</p>
+      <img class="image" src="https://media-exp1.licdn.com/dms/image/C4E03AQFz8xjqC2ZoTg/profile-displayphoto-shrink_200_200/0/1571752372683?e=1660176000&v=beta&t=W-1WY1BHc8OU4dTtWJdwHmlFFhCpmHhLz7gRUjKRxtM">
+    </div>
+    <div class="contact">
+      <h1 id="my-name">Contact Information</h1>
+      <a href=mailto:<essigm01@email.franklin.edu> "Email Rich"</a>
+      <p class="text">Call 1(800)438-5678 <b>(Get Lost)</b></p>
+      <br>
+      <a class="link" href="https://www.linkedin.com/in/rich-essigmann-49144b46/">My Linkedin</a>
+    </div>
+    <br>
+    <div class="container1" style="width: auto; height: auto;">
+      <div class="content1">
+        <center><img class="resize_fit_center" src="https://scontent.fcae1-1.fna.fbcdn.net/v/t1.6435-9/192830364_10219237467627784_8498966178117756160_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=s0HH8yYjVyEAX_Ev7Q7&_nc_ht=scontent.fcae1-1.fna&oh=00_AT8MJjULeYKZh-ZEnDnMUCBjPOjtuBCQKOJ6X3B6tN9Qag&oe=62E61EE8">
+      </div>
+    </div>
+</body>
